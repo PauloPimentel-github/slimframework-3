3a5133bfafd2fcd639f1c1f3fc08055389f89da2
PauloPimentel <paulo.h.g.pimentel@gmail.com>
2018-06-28 20:35:56 -0300

    Atualizando arquivo index.php, inserindo validações com OAuth 2.0

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 
+// error reporting (this is a demo, after all!)
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+require_once('oauth2/src/OAuth2/Autoloader.php');
+OAuth2\Autoloader::register();
+
+$dsn = 'mysql:dbname=oauth2;host=localhost';
+$username = 'root';
+$password = '';
+
+$storage = new OAuth2\Storage\Pdo(array('dsn' => $dsn, 'username' => $username, 'password' => $password));
+$server = new OAuth2\Server($storage);
+
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Firebase\JWT\JWT;
 
 require_once '_app/Config.inc.php';
 require_once '_app/Conn/Conn.class.php';
@@ -26,6 +41,73 @@ $app->add(new \Slim\Middleware\HttpBasicAuthentication([
     /** Whitelist - Protege todas as rotas e só libera as de dentro do array */
     //"passthrough" => ["/auth/liberada", "/admin/ping"],
 ]));
+
+$app->post('/oauth2/token', function (Request $request, Response $response, array $args) use($app) {
+
+    // your public key strings can be passed in however you like
+    $publicKey  = file_get_contents('oauth2/test/config/keys/id_rsa.pub');
+    $privateKey = file_get_contents('oauth2/test/config/keys/id_rsa');
+
+    // create storage
+    $storage = new OAuth2\Storage\Memory(array(
+        'keys' => array(
+            'public_key'  => $publicKey,
+            'private_key' => $privateKey,
+        ),
+        // add a Client ID for testing
+        'client_credentials' => array(
+            'CLIENT_ID' => array('client_secret' => 'CLIENT_SECRET')
+        ),
+    ));
+
+    $server = new OAuth2\Server($storage, array(
+        'use_jwt_access_tokens' => true,
+    ));
+
+    $server->addGrantType(new OAuth2\GrantType\ClientCredentials($storage)); // minimum config
+
+    // send the response
+    $token = $server->handleTokenRequest(OAuth2\Request::createFromGlobals())->send();
+
+    return $response->withHeader('Content-type', 'application/json')->withJson($token, STATUS_OK);
+
+});
+
+$app->get('/oauth2/teste-token', function (Request $request, Response $response, array $args) use($app) {
+
+    /* for a Resource Server (minimum config) */
+    $publicKey = file_get_contents('oauth2/test/config/keys/id_rsa.pub');
+
+    // no private key necessary
+    $keyStorage = new OAuth2\Storage\Memory(array('keys' => array(
+        'public_key'  => $publicKey,
+    )));
+
+    $server = new OAuth2\Server($keyStorage, array(
+        'use_jwt_access_tokens' => true,
+    ));
+
+    // verify the JWT Access Token in the request
+    if (!$server->verifyResourceRequest(OAuth2\Request::createFromGlobals())) {
+        $json = array(
+            'status' => 0,
+            'response' => 'A autenticação é necessária para obter a resposta solicitada',
+            'data' => []
+        );
+
+        return $response->withHeader('Content-type', 'application/json')->withJson($json, STATUS_UNAUTHORIZED);
+        exit("Failed");
+
+    } else {
+        $json = array(
+            'status' => 1,
+            'response' => 'Autenticado, token válido recurso aceito para consumo da api',
+            'data' => []
+        );
+
+        return $response->withHeader('Content-type', 'application/json')->withJson($json, STATUS_OK);
+    }
+});
 
 /**
 * <b>get</b>: Método responsável por resgatar informações das categorias e do site
